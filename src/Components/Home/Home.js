fce8c27377039334a2ceb69747f0ac197c95e744
Stacy Grenier <sngrenier@gmail.com>
2021-07-15 18:50:39 -0700

    home-links-added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -29,9 +29,21 @@ export default class Home extends Component {
                     </Link>
                 </div>
                 <div className="icon-links">
-                    <div className="home-icon"><img src="https://img.icons8.com/material-outlined/20/ffffff/open-envelope.png"/></div>
-                    <div className="home-icon"><img src="https://img.icons8.com/ios-glyphs/20/ffffff/linkedin-2--v1.png"/></div>
-                    <div className="home-icon"><img src="https://img.icons8.com/material-outlined/20/ffffff/github.png"/></div>
+                    <div className="home-icon">
+                        <a href="https://www.linkedin.com/in/stacy-grenier/">
+                            <img src="https://img.icons8.com/material-outlined/20/ffffff/open-envelope.png"/>
+                            </a>
+                        </div>
+                    <div className="home-icon">
+                        <a href="https://www.linkedin.com/in/stacy-grenier/">
+                            <img src="https://img.icons8.com/ios-glyphs/20/ffffff/linkedin-2--v1.png"/>
+                            </a>
+                        </div>
+                    <div className="home-icon">
+                        <a href="https://github.com/Sngrenier">
+                            <img src="https://img.icons8.com/material-outlined/20/ffffff/github.png"/>
+                            </a>
+                        </div>
                 </div>
                 </div>
                 </div>
