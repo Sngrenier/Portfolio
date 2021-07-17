aff005f742336829e295979d7a792cbe97715f8b
Stacy Grenier <sngrenier@gmail.com>
2021-07-17 16:38:00 -0700

    dev-and-design-ios-styling

--- src/Components/Skills/Dev.js
+++ src/Components/Skills/Dev.js
@@ -3,6 +3,7 @@ import './Dev.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import webdevbkgrnd from './webdevbkgrnd.png';
 import Navbar from '../Navbar/Navbar'
+import { Link } from 'react-router-dom'
 
 export default class dev extends Component {
     render() {
@@ -22,7 +23,7 @@ export default class dev extends Component {
                                 <div className="dot4"><p>.</p></div>
                             </div>
                             </div>
-                        <img alt="img-2" className="bkgrd-img" src={webdevbkgrnd} />
+                        <img alt="img-2" className="bkgrd-img-dev" src={webdevbkgrnd} />
                     
                     </div>
 
@@ -68,11 +69,28 @@ export default class dev extends Component {
                                 <li className="dev-bullet1">ASANA</li>
                             </ul>
                             </div>
-                            
                             </div>
-                    </div>
+
+                            <section className="container-fluid">
+                                <div className="project-overview-deskills">
+                                    <div className="project-overview-devskills">
+                                        <p className="proj-para-body-devskills">it's kind of awesome to build stuff right?!</p>
+                                        <p className="proj-para-1-devskills">check out my <a><Link to="gallery">PROJECTS</Link></a> page on this site to see some of my recent web development work. And if somehow my <a><Link to="gallery">PROJECTS</Link></a> page just isn't enough, you can also view the source code for this site as well as additional projects by clicking below.</p>
+                                    <div className="devskills-photos-btn">
+                                        <button className="devskills-photo-btn">
+                                            <a href="https://github.com/Sngrenier">
+                                            <span className="btn-type">source code</span>
+                                            </a>
+                                            <img alt="" className="git-icon" src="https://img.icons8.com/ios-glyphs/20/000000/github.png"/>
+                                        </button>
+                                    </div>
+                                    </div>
+                                </div>
+                            </section>
+
                 </div>
             </div>
+        </div>
         )
     }
 }
